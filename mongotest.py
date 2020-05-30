9f2e7c828beb4e3d49f2856f19f0f6c4e93510da
Ryan Stronge <ruanstronge@gmail.com>
2020-05-30 15:20:49 +0100

    Added processing

--- mongotest.py
+++ mongotest.py
@@ -1,16 +1,28 @@
-import pymongo
+import os
+import psycopg2
+
+conn = psycopg2.connect(dbname="ddcdvtofrshbnj", user="ntvhhmrhgzdmqh", password="70f5719386ca8d7a4464e7ba903ff81ddbe1fe1d444071cc5ce4e1ad28059870",
+                        host="ec2-54-247-89-181.eu-west-1.compute.amazonaws.com", port="5432")
+
+cur = conn.cursor()
 
 
-client = pymongo.MongoClient(
-    "mongodb://agile-sensing-analytics:w77pebv6@ds035995.mlab.com:35995/heroku_vgcclk7m?retryWrites=false")
+def execute_command(command):
+    try:
+        cur.execute(command)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
-db = client.imagedata
-db.command("createUser", "admin", pwd="password", roles=["root"])
-""" data = db.data
 
-testDocument = {
-    "url": "https://makerspacequb.github.io/images/QLab.png",
-    "points": "{\"name\":\"polyline\",\"all_points_x\":[537,625,649,539],\"all_points_y\":[641,778,652,640]}",
-    "anotherRow": "test"
-}
-data.insert_one(testDocument) """
+execute_command("SELECT * FROM data")
+rows = cur.fetchall()
+
+for row in rows:
+    print("Row: ")
+    print(str(row))
+    print("ID: " + str(row[0]))
+    print("URL: " + str(row[1]))
+    print("Points: " + str(row[2]))
+    print("Type: " + str(type(row)))
+    print("\n")
--- processing/processPolyline.py
+++ processing/processPolyline.py
@@ -1,73 +1,89 @@
-import csv
 import requests
 import json
-import ast
 from PIL import Image, ImageDraw
 import numpy as np
 import os
 import cv2
+import psycopg2
+import time
 
-print(os.listdir())
-results = []
-with open("csv/export.csv") as csvfile:
-    next(csvfile)
-    reader = csv.reader(csvfile)
-    for row in reader:
-        results.append(row)
+conn = psycopg2.connect(dbname="ddcdvtofrshbnj", user="ntvhhmrhgzdmqh", password="70f5719386ca8d7a4464e7ba903ff81ddbe1fe1d444071cc5ce4e1ad28059870",
+                        host="ec2-54-247-89-181.eu-west-1.compute.amazonaws.com", port="5432")
+cur = conn.cursor()
 
-    #results[x][0] is url
-    #results[x][5] is points
 
-    length = len(results)
+def execute_command(command):
+    try:
+        cur.execute(command)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return cur.fetchall()
 
-    for x in range(length):
-        fileName = "fullimgs/image"+str(x)+".jpg"
-        img_data = requests.get(results[x][0]).content
-        with open(fileName, 'wb') as handler:
+
+def getcsvdataprocess():
+    print(os.getcwd())
+    print(os.listdir())
+    now = time.strftime("%Y%m%d-%H%M%S")
+    name = "img"+str(now)
+    os.mkdir("fullimgs/"+name)
+    os.mkdir("newimgs/"+name)
+    execute_command("SELECT * FROM data")
+    rows = cur.fetchall()
+    for row in rows:
+        name = "img"+str(row[0])
+        file_name = "fullimgs/"+name+".jpg"
+        # row[0] is id: int
+        # row[1] is url: str
+        # row[2] is points: json
+
+        img_data = requests.get(str(row[1])).content
+        
+        with open(file_name, 'wb') as handler:
             handler.write(img_data)
+            jsonData = str(row[2])
+            jsonObj = json.loads(jsonData)
+            print(jsonObj)
+            objName = jsonObj["name"]
+            yPoints = jsonObj["all_points_y"]
+            xPoints = jsonObj["all_points_x"]
 
-        print(results[x][5])
-        jsonData = results[x][5]
-        jsonObj = json.loads(jsonData)
-        print(jsonObj)
-        name = jsonObj["name"]
-        yPoints = jsonObj["all_points_y"]
-        xPoints = jsonObj["all_points_x"]
+            yList = eval(str(yPoints))
+            xList = eval(str(xPoints))
 
-        yList = eval(str(yPoints))
-        xList = eval(str(xPoints))
+            if objName == "polyline":
+                im = Image.open(file_name).convert("RGBA")
+                imArray = np.asarray(im)
+                polygon = [None] * len(yList)
+                for z in range(len(yList)):
+                    polygon[z] = (xList[z], yList[z])
+                maskIm = Image.new(
+                    'L', (imArray.shape[1], imArray.shape[0]), 0)
+                ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
+                mask = np.array(maskIm)
+                newImArray = np.empty(imArray.shape, dtype='uint8')
+                newImArray[:, :, :3] = imArray[:, :, :3]
+                newImArray[:, :, 3] = mask*255
+                newIm = Image.fromarray(newImArray, "RGBA")
+                newIm.save("newimgs/"+name+"/"+name + ".png")
+                im = cv2.imread("newimgs/"+name+"/" +
+                                name+".png", cv2.IMREAD_UNCHANGED)
+                y, x = im[:, :, 3].nonzero()
+                minx = np.min(x)
+                miny = np.min(y)
+                maxx = np.max(x)
+                maxy = np.max(y)
 
-        if name == "polyline":
-            im = Image.open(fileName).convert("RGBA")
-            imArray = np.asarray(im)
-            polygon = [None] * len(yList)
-            for z in range(len(yList)):
-                polygon[z] = (xList[z], yList[z])
-            print(polygon)
-            maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
-            ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
-            mask = np.array(maskIm)
-            newImArray = np.empty(imArray.shape, dtype='uint8')
-            newImArray[:, :, :3] = imArray[:, :, :3]
-            newImArray[:, :, 3] = mask*255
-            newIm = Image.fromarray(newImArray, "RGBA")
-            newIm.save("newimgs/image"+str(x)+".png")
+                cropImg = im[miny:maxy, minx:maxx]
+                whiteCellsMask = np.logical_and(cropImg[:, :, 0] == 255, np.logical_and(
+                    cropImg[:, :, 1] == 255, cropImg[:, :, 2] == 255))
+                cropImg[whiteCellsMask, :] = [255, 255, 255, 0]
 
-            # cropping
-            im = cv2.imread("AgileMTurk/processing/newimgs/image" +
-                            str(x)+".png", cv2.IMREAD_UNCHANGED)
-            y, x = im[:, :, 3].nonzero()
-            minx = np.min(x)
-            miny = np.min(y)
-            maxx = np.max(x)
-            maxy = np.max(y)
+                cv2.imwrite("newimgs/"+name+"/" +
+                            name + ".png", cropImg)
+                cv2.waitKey(0)
+            else:
+                print("skipping header")
 
-            cropImg = im[miny:maxy, minx:maxx]
-            whiteCellsMask = np.logical_and(cropImg[:, :, 0] == 255, np.logical_and(
-                cropImg[:, :, 1] == 255, cropImg[:, :, 2] == 255))
-            cropImg[whiteCellsMask, :] = [255, 255, 255, 0]
-            cv2.imwrite("AgileMTurk/processing/newimgs/image" +
-                        str(x)+".png", cropImg)
-            cv2.waitKey(0)
-        else:
-            print("Not a polyline")
+
+getcsvdataprocess()
